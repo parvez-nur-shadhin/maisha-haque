4bcf240540a720a127098faa4ff427a836ee3806
parvez-nur-shadhin <parveznur00@gmail.com>
2026-04-02 17:00:36 +0600

    Animation Added

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import BannerIMG from "../../assets/maisha-haque.jpg";
 import { Fade } from "react-awesome-reveal";
+import './banner.css';
 
 const Banner = () => {
   return (
-    <div id="about" className="max-w-300 mx-auto p-4 flex flex-col-reverse md:flex-row justify-between items-center mt-4 md:mt-15 text-center md:text-left">
+    <div id="about" className="max-w-250 mx-auto p-4 flex flex-col-reverse md:flex-row justify-between items-center mt-4 md:mt-15 text-center md:text-left">
       <div className="space-y-5 fade-in duration-700">
         <h1 className="text-4xl font-bold">
           Hello, I'm <span className="text-blue-600">Maisha</span>
@@ -19,7 +20,7 @@ const Banner = () => {
           the pursuit of excellence in everything I do.
         </p>
       </div>
-      <div className="mb-5">
+      <div className="fade-in mb-5 duration-700">
         <img className="max-w-70 rounded-xl transform hover:scale-110 duration-500 cursor-pointer" src={BannerIMG} alt="" />
       </div>
     </div>
