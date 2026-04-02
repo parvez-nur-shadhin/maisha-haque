3a123d786dc7dc304f5a80140cf884664b1f318a
parvez-nur-shadhin <parveznur00@gmail.com>
2026-04-02 23:35:55 +0600

    Footer Added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./Components/Navbar/Navbar";
 import Banner from "./Components/Banner/Banner"
 import Education from "./Components/Education/Education";
 import Experience from "./Components/Experience/Experience";
+import Footer from "./Components/Footer/Footer";
 function App() {
 
   return (
@@ -10,6 +11,7 @@ function App() {
       <Banner />
       <Education />
       <Experience />
+      <Footer />
     </>
   )
 }
